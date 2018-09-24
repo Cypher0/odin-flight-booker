b37e9a45d74a1eae94f2b2a0874f7804e527e4da
Raiko Murulauk <raiko4238@gmail.com>
2018-09-24 15:41:34 +0300

    fix host for production

--- app/mailers/passenger_mailer.rb
+++ app/mailers/passenger_mailer.rb
@@ -3,7 +3,7 @@ class PassengerMailer < ApplicationMailer
   def thank_you_email
     @passenger = params[:passenger]
     @booking = params[:booking]
-    @url = booking_url(@booking, :host => 'example.com')
+    @url = booking_url(@booking)
     mail(to: @passenger.email,
          subject: 'You have booked your ticket.')
   end
